05feb2e7dcc1f720e10249ea02a9ee05765fba09
Aram <ameanarm@gmail.com>
2023-03-05 17:23:52 +0400

    fff

--- js/index.js
+++ js/index.js
@@ -7,13 +7,16 @@ const dogImage = document.querySelector(".dog-img");
 const catImage = document.querySelector(".cat-img");
 const button = document.querySelector(".generete-btn");
 
-
 async function fetchRequest(url) {
-  return fetch(url)
-  .then((response) => response.json())
-  .then((data) => data)
+  try {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => data);
+  } catch (error) {
+    console.log(error);
+  }
 }
-button.addEventListener('click', async () => {
+button.addEventListener("click", async () => {
   let userResponse = await fetchRequest(USER_API);
   let dogResponse = await fetchRequest(DOG_API);
   let catResponse = await fetchRequest(CAT_API);
@@ -21,12 +24,4 @@ button.addEventListener('click', async () => {
   catImage.src = catResponse.file;
   dogImage.src = dogResponse.message;
   userImage.src = userResponse.results[0].picture.large;
-  // console.log(result);
-})
-
-
-
-
-
-
-
+});
